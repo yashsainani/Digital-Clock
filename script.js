593297eb1a21af3b2231c264c87849e31a5be541
yashsainani <yashsainani1803@gmail.com>
2024-11-06 17:55:55 +0530

    Created DIGITAL CLOCK

---
+++ script.js
@@ -0,0 +1,27 @@
+const hour = document.getElementById('hour');
+const minute = document.getElementById('minute');
+const second = document.getElementById('second');
+const meridiem = document.getElementById('meridiem');
+
+function clock() {
+    const currTime = new Date();
+    let currHour = currTime.getHours();
+    let currMinute = currTime.getMinutes();
+    let currSeconds = currTime.getSeconds();
+    meridiem.textContent = currHour >= 12 ? 'PM' : 'AM';
+    currHour %= 12;
+    currHour = converting(currHour);
+    currMinute = converting(currMinute);
+    currSeconds = converting(currSeconds);
+    hour.textContent = currHour;
+    minute.textContent = currMinute;
+    second.textContent = currSeconds;
+}
+
+function converting(val) {
+    return val < 10 ? '0' + val : val;
+}
+
+clock();
+
+setInterval(clock, 1000);
